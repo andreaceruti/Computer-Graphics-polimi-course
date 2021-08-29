21f54fa0836645fdf1af92d17acd34ed98d90730
Andrea <44469014+andreaceruti@users.noreply.github.com>
2021-08-29 15:52:37 +0200

    paddle correctly debugged

--- logic/constants.js
+++ logic/constants.js
@@ -6,7 +6,9 @@ let PADDLE_SPEED = 0.013;
 const PADDLE_Y = 14;
 const BALL_Y = 12;
 
-const PADDLE_WALL_PADDING = 0.3;
+const PADDLE_HALF_LENGTH = 2;
+const WALL_LATERAL_HALF_LENGTH = 2;
+const PADDLE_WALL_PADDING = 0;
 const BALL_DEATH_PADDING = 2;
 
 // camera defaults
