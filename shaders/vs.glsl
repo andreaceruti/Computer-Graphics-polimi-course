b82a533afbba3f25bf5e4c2cf2de196b8eb45724
Andrea <44469014+andreaceruti@users.noreply.github.com>
2021-09-01 12:50:10 +0200

    aa

--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -6,17 +6,18 @@ in vec3 inPosition;
 in vec3 inNormal;
 in vec2 in_uv;
 
-out vec3 fsNormal;
-out vec2 fsUV;
-out vec3 fs_pos; 
-
 uniform mat4 matrix; //worldViewProjection matrix to draw objects
 uniform mat4 nMatrix; //matrix to transform normals in world space shading
 uniform mat4 pMatrix; //matrix to transform positions in world space shading
 
+out vec3 fsNormal;
+out vec2 fsUV;
+out vec3 fs_pos; 
+
 void main() {
   fsUV = in_uv;
-  fs_pos = (pMatrix * vec4(inPosition, 1.0)).xyz;
+  fs_pos = mat3(pMatrix) * inPosition;
+  //fs_pos = (pMatrix * vec4(inPosition, 1.0)).xyz;
   fsNormal = mat3(nMatrix) * inNormal;
   //fsNormal = (nMatrix * vec4(inNormal, 0.0)).xyz;
 
